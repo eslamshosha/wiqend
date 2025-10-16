82141e1c20a28ab54e07ea6eaff923518f793a9d
eslam shosha <eslamshosha22.es@gmail.com>
2025-10-16 17:31:46 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+  //phone size menu onclick
+  $("#menu-id").click(function (e) {
+    e.preventDefault();
+    $(".navgition").toggleClass("reset-left");
+    $("body").toggleClass("overflow");
+  });
+  $(".nav-head .close-menu").click(function () {
+    $(".navgition").removeClass("reset-left");
+    $("body").removeClass("overflow");
+  });
+  ///////// ** main** /////////
+  var mainSlider = new Swiper(".main-slider .swiper-container", {
+    loop: false,
+    autoplay: true,
+    pagination: {
+      el: ".main-slider .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".main-slider .swiper-btn-next",
+      prevEl: ".main-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      767: {
+        slidesPerView: 1,
+      },
+      992: {
+        slidesPerView: 1,
+      },
+      1199: {
+        slidesPerView: 1,
+      },
+    },
+  });
+  //////////** fixed arrow to top**//////////
+  $(".arrow-top").click(function () {
+    $("html").css("scroll-behavior", "unset");
+
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000,
+      "swing"
+    );
+    setTimeout(() => {
+      $("html").css("scroll-behavior", "smooth");
+    }, 1000);
+  });
+  $(this).scrollTop() >= 500
+    ? $(".arrow-top").fadeIn(300)
+    : $(".arrow-top").fadeOut(300);
+
+  $(window).scroll(function () {
+    $(this).scrollTop() >= 500
+      ? $(".arrow-top").fadeIn(300)
+      : $(".arrow-top").fadeOut(300);
+  });
+
+  var slider2 = new Swiper(".booking-card .swiper", {
+    loop: true,
+    autoplay: true,
+    preloadImages: false,
+    slidesPerView: 1,
+    pagination: {
+      el: ".booking-card .swiper-pagination",
+      clickable: true,
+    },
+  });
+});
